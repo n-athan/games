b47b01a6e9436bf0669437b0e0258da90977782f
n-athan <dump.nathan@gmail.com>
2018-12-03 19:50:28 +0100

    cards, class and functions added

---
+++ memory/cards.js
@@ -0,0 +1,60 @@
+function Card(i,p) {
+    this.id = i;
+    this.pair = p;
+    this.color = p*47;
+    this.letter = alphabet[p];
+
+    this.memorize = function() {
+        if (prc(initialMemory)) {
+            console.log(this.id, " memorized")
+            botMemory.push(this);
+        } else console.log("card not memorized");
+    }
+
+    this.toHTML = function() {
+        return `<div class="card" onclick="cardClick(${this.id})" id="${this.id}"><p style="color:hsl(${this.color},80%,50%);">${this.letter}</p></div>`
+    }
+
+}
+
+function forget() {
+    for (var c = 0; c < botMemory.length; c++){
+        if (prc(forgetfulness)) {
+            console.log(botMemory[c].id, " forgotten");
+            remove(botMemory,botMemory[c]);
+        }
+    }
+}
+
+function newSet(number) {
+    let set = [];
+    let p = 0;
+    for (var i = 0; i < number; i += 2) {
+        var card = new Card(i,p);
+        set.push(card);
+        var card = new Card(i+1,p);
+        set.push(card);
+        p++;
+    }
+    return set;
+}
+
+//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
--- memory/game.js
+++ memory/game.js
@@ -1,7 +1,74 @@
+let state;
+//states: playerNoCardFlipped, playerOneCardFlipped, playerTwoCardFlipped, playerPlayed, botPlayed. 
+let initialMemory = document.getElementById("initialMemory").value;
+let forgetfulness = document.getElementById("forgetfulness").value;
+let botMemory = [];
+let allCards;
+let alphabet = ("abcdefghijklmnopqrstuvwxyz").toUpperCase().split("");
 
+setup(24);
 
 // random function, returns true $percent of the time, else false
-function prc(percent){
+function prc(percent) {
     let a = Math.random();
-    return (a < 100/percent);
+    return (a < percent/100);
 }
+
+//remove function
+function remove(array, element) {
+    const index = array.indexOf(element);
+    array.splice(index, 1);
+}
+
+//start a game 
+function setup(n) {
+    if (n%2 == 1) {alert("there must be an even number of cards to start a game"); return false}
+    allCards = shuffle(newSet(n));
+    let div = document.getElementById("field");
+    for (var c = 0; c < allCards.length; c++) {
+        div.insertAdjacentHTML('beforeend', allCards[c].toHTML());
+    }
+    state = "playerNoCardFlipped"
+}
+
+//click on a card
+function cardClick(id) {
+    //testing stateless
+    // console.log(id);
+    // let ct = document.getElementById(id).firstChild;
+    // ct.classList.add("open");
+    // allCards.find(x => x.id === id).memorize();
+
+    //get state of game
+    if (state == "playerNoCardFlipped") {
+        //flip clicked card
+        let c = document.getElementById(id).firstChild;
+        c.classList.add("open");
+        state = "playerOneCardFlipped";
+        allCards.find(x => x.id === id).memorize();
+    } else if (state == "playerOneCardFlipped") {
+        //flip clicked card
+        let c = document.getElementById(id).firstChild;
+        c.classList.add("open");
+        state = "playerTwoCardFlipped";
+        allCards.find(x => x.id === id).memorize();
+    } else if (state == "playerTwoCardFlipped") {
+        // unflip all cards
+        let cards = document.getElementsByClassName("card");
+        for (var c = 0; c < cards.length; c++) {
+            cards[c].firstChild.classList.remove("open");
+        }
+        // state = "playerPlayed";
+        state = "playerNoCardFlipped";
+        forget();
+    } else if (state == "botPlayed") {
+        // unflip all cards
+        let cards = document.getElementsByClassName("card").firstChild;
+        for (var c = 0; c < cards.length; c++) {
+            cards[c].firstChild.classList.remove("open");
+        }
+        state = "playerNoCardFlipped";
+        forget();
+    }
+}
+
