bed8b5147acb0f485889535285393dd5f652d03d
Nathan Vos <dump.nathan@gmail.com>
2019-01-09 20:29:36 +0100

    improved game and design

--- memory/cards.js
+++ memory/cards.js
@@ -1,18 +1,20 @@
+let colorOffset = Math.floor(Math.random()*40);
+
 function Card(i,p) {
     this.id = i;
     this.pair = p;
-    this.color = p*47;
+    this.color = (p*55+colorOffset)%360;
     this.letter = alphabet[p];
 
     this.memorize = function() {
-        if (prc(initialMemory)) {
+        if (prc(initialMemory)&&botMemory.indexOf(this) == -1) {
             console.log(this.id, " memorized")
             botMemory.push(this);
         } else console.log("card not memorized");
     }
 
     this.toHTML = function() {
-        return `<div class="card" onclick="cardClick(${this.id})" id="${this.id}"><p style="color:hsl(${this.color},80%,50%);">${this.letter}</p></div>`
+        return `<div class="card" unselectable="on" onclick="cardClick(${this.id})" id="${this.id}"><p style="color:hsl(${this.color},80%,50%);">${this.letter}</p></div>`
     }
 
 }
@@ -37,6 +39,25 @@ function newSet(number) {
         p++;
     }
     return set;
+}
+
+function removePair(a,b) {
+    let d1 = document.getElementById(a.id);
+    let d2 = document.getElementById(b.id);
+    d1.style.visibility="hidden";
+    d1.childNodes[0].remove();
+    d2.style.visibility="hidden";
+    d2.childNodes[0].remove();
+}
+
+function unflip() {
+    let cards = document.getElementsByClassName("card");
+    for (var c = 0; c < cards.length; c++) {
+        if (cards[c].firstChild){
+            cards[c].firstChild.classList.remove("open");
+        }
+    }
+    flipped=[];
 }
 
 //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
--- memory/game.js
+++ memory/game.js
@@ -1,12 +1,16 @@
 let state;
-//states: playerNoCardFlipped, playerOneCardFlipped, playerTwoCardFlipped, playerPlayed, botPlayed. 
+//states: playerNoCardFlipped, playerOneCardFlipped, playerPlayed, botPlayed. 
 let initialMemory = document.getElementById("initialMemory").value;
 let forgetfulness = document.getElementById("forgetfulness").value;
 let botMemory = [];
 let allCards;
 let alphabet = ("abcdefghijklmnopqrstuvwxyz").toUpperCase().split("");
+let scorePlayer = 0;
+let scoreBot = 0;
+let flipped = [];
+let ignoreMouse = false;
 
-setup(24);
+setup(12);
 
 // random function, returns true $percent of the time, else false
 function prc(percent) {
@@ -33,6 +37,7 @@ function setup(n) {
 
 //click on a card
 function cardClick(id) {
+    if (ignoreMouse){return}
     //testing stateless
     // console.log(id);
     // let ct = document.getElementById(id).firstChild;
@@ -44,31 +49,66 @@ function cardClick(id) {
         //flip clicked card
         let c = document.getElementById(id).firstChild;
         c.classList.add("open");
-        state = "playerOneCardFlipped";
-        allCards.find(x => x.id === id).memorize();
+        // find card object that matches flipped DOM-element, to memorize.
+        let f = allCards.find(x => x.id === id);
+        flipped.push(f);
+        state = "playerOneCardFlipped";        
+        f.memorize();
+
     } else if (state == "playerOneCardFlipped") {
+        //ensure you don't click on same card twice.
+        if (id == flipped[0].id) {return}
         //flip clicked card
         let c = document.getElementById(id).firstChild;
         c.classList.add("open");
-        state = "playerTwoCardFlipped";
-        allCards.find(x => x.id === id).memorize();
-    } else if (state == "playerTwoCardFlipped") {
-        // unflip all cards
-        let cards = document.getElementsByClassName("card");
-        for (var c = 0; c < cards.length; c++) {
-            cards[c].firstChild.classList.remove("open");
-        }
-        // state = "playerPlayed";
-        state = "playerNoCardFlipped";
-        forget();
+        // find card object that matches flipped DOM-element, to memorize.
+        let f = allCards.find(x => x.id === id);
+        f.memorize();
+        flipped.push(f);
+        checkSimilar(); //changes state of game if pair is flipped.
+        if (state === "playerOneCardFlipped") {
+            ignoreMouse = true;
+            setTimeout(function() {
+                unflip();
+                state = "playerNoCardFlipped"; //change to botTurn after testing.
+                ignoreMouse = false;
+            },1000);            
+            forget();
+            //add function to start bots turn
+        };
+
     } else if (state == "botPlayed") {
-        // unflip all cards
-        let cards = document.getElementsByClassName("card").firstChild;
-        for (var c = 0; c < cards.length; c++) {
-            cards[c].firstChild.classList.remove("open");
-        }
+        setTimeout(unflip(),1000);
         state = "playerNoCardFlipped";
         forget();
     }
+    console.log(state);
 }
 
+function checkSimilar() {
+    let a = flipped[0];
+    let b = flipped[1];
+    if (a.pair == b.pair) {
+        if (state.indexOf("player") !== -1) {
+            scorePlayer+=1;
+            ignoreMouse= true;
+            setTimeout(function() {
+                removePair(a,b);
+                state = "playerNoCardFlipped";
+                ignoreMouse = false;
+            },500);    
+            updateScore("Player", scorePlayer)
+        } else {
+            scoreBot+=1;
+            state == "botTurn"; 
+            updateScore("Bot", scoreBot)            
+            //add function to start bots turn
+        }    
+    } //else do nothing
+    flipped = [];
+}
+
+function updateScore(person, score) {
+    let p = document.getElementById(`score${person}`);
+    p.innerHTML = person + ": " + score;
+}
